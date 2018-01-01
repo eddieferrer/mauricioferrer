5b59e38616f04dbd6583df39d7f39a4fb542d982
Eddie Ferrer <eferrer@gmail.com>
2018-01-01 09:05:31 -0800

    fix email send

--- php/form-process.php
+++ php/form-process.php
@@ -50,8 +50,12 @@ $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
 
+$headers  = "From: Admin <admin@mauricioferrer.com>" . "\r\n";
+$headers .= 'MIME-Version: 1.0' . "\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From: admin@sizesquirrel.com");
+$success = mail($EmailTo, $Subject, $Body, $headers);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
