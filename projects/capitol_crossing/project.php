a44dc3bf01398b6479a8fbb84af5dd9fff5a64d2
Eddie Ferrer <eferrer@gmail.com>
2018-01-01 08:30:46 -0800

    added footer and head includes

--- projects/capitol_crossing/project.php
+++ projects/capitol_crossing/project.php
@@ -2,40 +2,8 @@
 <html lang="en">
 
 <head>
+<?php include '../../includes/head.html';?>
 
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Mauricion Ferrer - RA, LEED AP">
-<meta name="keywords" content="Mauricio Ferrer" />
-<meta name="author" content="Mauricio Ferrer">
-
-<!-- favicon -->
-<link rel="icon" href="/images/favicon.png">
-<!-- page title -->
-<title>MAURICIO FERRER - RA, LEED AP</title>
-<!-- bootstrap css -->
-<link href="/css/bootstrap.min.css" rel="stylesheet">
-<!-- css -->
-<link href="/css/style.css" rel="stylesheet">
-<link href="/css/animate.css" rel="stylesheet">
-<link href="/css/magnific-popup.css" rel="stylesheet">
-
-<link rel="stylesheet" href="/css/mf_style.css">
-
-
-<!-- fonts -->
-<link href="https://fonts.googleapis.com/css?family=Raleway:400,700,800,900" rel="stylesheet">
-<link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link href='/fonts/FontAwesome.otf' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="/css/linear-icons.css">
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 <style>
 #header-banner {
   background: url("banner.jpg") no-repeat;
@@ -134,45 +102,8 @@
 </div><!-- / container -->
 <!-- / content -->
 
-<!-- footer -->
-<footer>
-    <div class="container">
-        <p class="footer-info">© MAURICIO FERRER</p>
-    </div><!-- / container -->
-</footer>
-<!-- / footer -->
+<?php include '../../includes/footer_with_scripts.html';?>
 
-<!-- javascript -->
-<script src="/js/jquery.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/jquery.easing.min.js"></script>
-
-<!-- portfolio -->
-<script src="/js/custom.js"></script>
-<script src="/js/jquery.shuffle.min.js"></script>
-<!-- / portfolio -->
-
-<!-- lightbox -->
-<script src="/js/jquery.magnific-popup.min.js"></script>
-<script type="text/javascript">
-// This will create a single gallery from all elements that have class "lightbox"
-$('.lightbox').each(function() {
-    $(this).magnificPopup({
-        delegate: 'a.project_image', // the selector for gallery item
-        type: 'image',
-        gallery: {
-          enabled:true
-        }
-    });
-});
-</script>
-<!-- / lightbox -->
-
-<!-- preloader -->
-<script src="/js/preloader.js"></script>
-<!-- / preloader -->
-
-<!-- / javascript -->
 </body>
 
 </html>
